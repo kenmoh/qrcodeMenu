ee2252747a73e9831f937d6fc8b1c42e510b5a8a
kenmoh <kenneth.aremoh@gmail.com>
2022-08-01 17:06:13 +0100

    remove generate pdf button

--- menugenerator/urls.py
+++ menugenerator/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('home/', include('menu.urls')),
+    path('', include('menu.urls')),
     path('admin/', admin.site.urls),
 ]
