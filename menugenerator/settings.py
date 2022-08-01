f97114b51137bc10eb1df3818d9a401fb8a3aa62
kenmoh <kenneth.aremoh@gmail.com>
2022-08-01 16:18:36 +0100

    installed gunicorn, whitenoise

--- menugenerator/settings.py
+++ menugenerator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vg=v-)(up+&f@+chp8-cz8!p5%#dti7c1k1ait9+fsmxm^oy-4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,10 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    Path(BASE_DIR, 'static')
-]
+STATIC_ROOT = Path(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#     Path(BASE_DIR, 'static')
+# ]
 
 MEDIA_URL = '/img/'
 MEDIA_ROOT = Path(BASE_DIR, 'static/img')
